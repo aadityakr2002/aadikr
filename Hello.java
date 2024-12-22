eda8cbe7d2a82fc176e4a66301eb50f2752d6ccd
Aaditya Kumar <aadityakumar@Aadityas-MacBook-Pro-2.local>
2024-12-22 15:14:46 -0800

    Initial commit for Lab 02

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, this is Aaditya Kumar.");
+        System.out.println("This is an example of a team collaboration exercise.");
+        System.out.println("Additional line added for simulation purposes.");
+    }
+}
